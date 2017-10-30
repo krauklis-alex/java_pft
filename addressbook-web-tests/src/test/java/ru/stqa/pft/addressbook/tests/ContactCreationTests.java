5904c12cb12ef4c2dfa82c2d3ab0b48ec5c44415
krauklis-alex <kolpakova.1203@gmail.com>
2017-10-31 00:47:39 +0300

    Вспомогательные классы переехали в AppManager, TestBase делегирует App (не наследует)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
@@ -16,10 +8,10 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
 
-        initContactCreation();
-        fillContactForm(new ContactData("first-name-test", "last-name-test", "2222222", "test@test.te", "NewYork Central park", "1234567890", "homepage.com", "note text"));
-        submitContactCreation();
-        returnToHomePage();
+        app.initContactCreation();
+        app.fillContactForm(new ContactData("first-name-test", "last-name-test", "2222222", "test@test.te", "NewYork Central park", "1234567890", "homepage.com", "note text"));
+        app.submitContactCreation();
+        app.returnToHomePage();
     }
 
 }
