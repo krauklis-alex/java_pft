52bf0207c16077208c35bc9a172363ba99073c39
krauklis-alex <kolpakova.1203@gmail.com>
2017-10-30 23:55:13 +0300

    Создана двухуровневая архитектура тестов групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase {
+
+  public GroupHelper(FirefoxDriver wd) {
+
+    super(wd);
+  }
+
+  public void returnToGroupPage() {
+
+    click(By.linkText("group page"));
+  }
+
+  public void submitGroupCreation() {
+
+    click(By.name("submit"));
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.getName());
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
+
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
+
+  public void deleteSelectedGroups() {
+    click(By.xpath("//div[@id='content']/form/input[5]"));
+  }
+
+  public void selectGroup() {
+      if (!wd.findElement(By.xpath("//div[@id='content']/form/span[10]/input")).isSelected()) {
+        click(By.xpath("//div[@id='content']/form/span[10]/input"));
+      }
+  }
+}
