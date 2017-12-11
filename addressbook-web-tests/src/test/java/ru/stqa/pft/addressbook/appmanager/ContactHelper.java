855eaaca08d4236465b31a34f76f55d989c00f63
krauklis-alex <kolpakova.1203@gmail.com>
2017-12-11 22:24:26 +0300

    Добавлена проверка по длине списков в  тесте создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,12 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContactHelper extends HelperBase {
@@ -65,4 +67,18 @@ public class ContactHelper extends HelperBase {
    fillContactForm(contact, creation);
    submitContactCreation();
   }
-}
+
+  public List<ContactData> getContactList() {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("tr[name='entry']"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      //int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
+      ContactData contact = new ContactData (name, null, null, null,
+              null, null, null, null, null);
+      contacts.add(contact);
+    }
+    return contacts;
+  }
+ }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,16 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    app.getContactHelper().createContact(new ContactData("first-name-test", "last-name-test",
+    List<ContactData> before = app.getContactHelper().getContactList();
+    ContactData contact = new ContactData("first-name-test", "last-name-test",
             "2222222", "test@test.te", "NewYork Central park", "1234567890",
-            "homepage.com", "note text", "test1"), true);
+            "homepage.com", "note text", "test1");
+    app.getContactHelper().createContact(contact, true);
     app.getNavigationHelper().returnToHomePage();
-  }
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+    }
 
 }
