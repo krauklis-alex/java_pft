ba2c7e59cf2444c87045fd3d5c337edaa313bac6
krauklis-alex <kolpakova.1203@gmail.com>
2017-12-13 00:48:52 +0300

    добавлена проверка на равенство списков контактов до и после удаления

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -12,26 +12,27 @@ public class ContactData {
   private int id;
   private String group;
 
-  public ContactData(int id, String firstname, String lastname, String homePhone,
-                     String email, String address, String mobilePhone,
-                     String homepage, String note, String group) {
-    this.id = id;
+//  public ContactData(int id, String firstname, String lastname, String homePhone,
+//                     String email, String address, String mobilePhone,
+//                     String homepage, String note, String group) {
+//    this.id = id;
+//
+//    this.firstname = firstname;
+//    this.lastname = lastname;
+//    this.homePhone = homePhone;
+//    this.email = email;
+//    this.address = address;
+//    this.mobilePhone = mobilePhone;
+//    this.homepage = homepage;
+//    this.note = note;
+//    this.group = group;
+//  }
 
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.homePhone = homePhone;
-    this.email = email;
-    this.address = address;
-    this.mobilePhone = mobilePhone;
-    this.homepage = homepage;
-    this.note = note;
-    this.group = group;
-  }
 
   public ContactData(String firstname, String lastname, String homePhone,
                      String email, String address, String mobilePhone,
                      String homepage, String note, String group) {
-    this.id = Integer.MAX_VALUE;
+//    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.homePhone = homePhone;
@@ -81,6 +82,14 @@ public class ContactData {
 
   public int getId() {
     return id;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
   }
 
   @Override
