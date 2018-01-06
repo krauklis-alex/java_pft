12271071ed684a05dfe29673ddb549d77611109b
krauklis-alex <kolpakova.1203@gmail.com>
2018-01-06 16:18:24 +0300

    Реализовано чтение тестовых данных из файла XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,17 +21,17 @@ public class ContactCreationTests extends TestBase {
   public Iterator<Object[]> validContacts() throws IOException {
     File photo = new File("src/test/resources/kim.jpg");
     List list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/contacts.csv")));
+    BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
-              .withHomePhone(split[3]).withEmail(split[4]).withHomepage(split[5]).withGroup(split[6])});
-//              .withPhoto(split[7])});
+      xml += line;
       line = reader.readLine();
     }
-
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> groups = (List<ContactData>) xstream.fromXML(xml);
+    return groups.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
   }
 
 
